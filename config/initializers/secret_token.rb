f3c627d742da8899ff3f2f31e18329f3ace08383
vm <vm@vm-Satellite-A500.(none)>
2012-05-02 10:29:24 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '1f5878769e52a41227bfa4a9798ffadf922e28d775a7dc15e5d783bdbc5598b5d5012e7d6ddd93fcd6e3f2bcda557bd9fe56c93a6505db78b94061cfa6295495'
